298260b75908b617e0d954a5a54bad5aaa61fa69
yashikajaincode <yashikajain2004@gmail.com>
2025-07-24 18:54:49 +0530

    Initial commit

---
+++ 763.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = {ch: i for i, ch in enumerate(s)}
+        res = []
+        start = end = 0
+        
+        for i, ch in enumerate(s):
+            end = max(end, last[ch])
+            if i == end:
+                res.append(end - start + 1)
+                start = i + 1
+                
+        return res
